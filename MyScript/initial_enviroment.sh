92d4b67c6e6bc369636eaf36460a61e4015e1e2f
junlinp <junlinp@deepmirror.com>
2025-04-17 14:01:34 +0800

    add initial_environment.sh

---
+++ MyScript/initial_enviroment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define mirror URL
+MIRROR_URL="https://pypi.tuna.tsinghua.edu.cn/simple"
+
+# Create pip config directory if not exists
+mkdir -p ~/.pip
+
+# Write the pip.conf
+cat <<EOF > ~/.pip/pip.conf
+[global]
+index-url = ${MIRROR_URL}
+timeout = 60
+EOF
+
+pip install -e .
+
