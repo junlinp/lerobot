43592cd81c5e23a4f80c3f3ee44b8b97c31960a8
junlinp <junlinp@deepmirror.com>
2025-04-18 14:21:10 +0800

    remove debug print

--- lerobot/scripts/control_robot.py
+++ lerobot/scripts/control_robot.py
@@ -265,10 +265,9 @@ def record(
             image_writer_processes=cfg.num_image_writer_processes,
             image_writer_threads=cfg.num_image_writer_threads_per_camera * len(robot.cameras),
         )
-    print(f"dataset.meta : {dataset.meta.features}")
-    print(f"dataset.features : {dataset.features}")
+    #print(f"dataset.meta : {dataset.meta.features}")
+    #print(f"dataset.features : {dataset.features}")
     if cfg.policy is not None:
-        print(f"policy input_features : {cfg.policy.input_features}")
         # asign the feature size from policy
         for key, policy_feature in cfg.policy.input_features.items():
             dataset.meta.features[key]['shape'] = policy_feature.shape
